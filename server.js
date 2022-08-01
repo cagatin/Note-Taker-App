dc14f987dd0274418ba39c29ce6438894bd90c1d
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-01 08:45:37 -0700

    imported path module

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Heroku --> https://evening-eyrie-72911.herokuapp.com/ 
 const express = require('express');
+const path = requie('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
