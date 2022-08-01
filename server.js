c13ffede757be82bcf02f42ae0eef56f3b2bf17c
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-01 08:43:55 -0700

    Added middlewear to body parsing and serving static files

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// Middlewear
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// serve the static files
+app.use(express.static('public'));
